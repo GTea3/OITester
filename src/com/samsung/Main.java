ea7bccf079f9907eba7baa8986f9a60233dd8222
Grzegorz Skowinski <g.skowinski@samsung.com>
2019-10-17 16:30:17 +0200

    Added Trojkaty challenge.

--- src/com/samsung/Main.java
+++ src/com/samsung/Main.java
@@ -2,6 +2,7 @@ package com.samsung;
 
 import com.samsung.solutions.Plakatowanie;
 import com.samsung.solutions.Krazki;
+import com.samsung.solutions.Trojkaty;
 import com.samsung.tester.Tester;
 
 import java.io.*;
@@ -10,5 +11,6 @@ public class Main {
     public static void main(String[] args) throws IOException {
         Tester.Test("Plakatowanie", Plakatowanie::main);
         Tester.Test("Krazki", Krazki::main);
+        Tester.Test("Trojkaty", Trojkaty::main);
     }
 }
--- src/com/samsung/solutions/Plakatowanie.java
+++ src/com/samsung/solutions/Plakatowanie.java
@@ -1,6 +1,32 @@
 package com.samsung.solutions;
 
+import java.util.Scanner;
+import java.util.Stack;
+
 public class Plakatowanie {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+
+        Stack<Integer> s = new Stack<>();
+        int result = 0;
+
+        while(n-- > 0) {
+            scanner.nextInt();
+            int h = scanner.nextInt();
+            if(s.empty() || h > s.peek())
+                s.push(h);
+            else if(h < s.peek()) {
+                while(!s.empty() && h < s.peek()) {
+                    s.pop();
+                    ++result;
+                }
+                if(s.empty() || h > s.peek())
+                    s.push(h);
+            }
+        }
+        result += s.size();
+
+        System.out.println(result);
     }
 }
