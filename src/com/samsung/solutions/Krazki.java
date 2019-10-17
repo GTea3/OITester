dc8e845da9396eb7a041f05aca037e18bf5e577a
Grzegorz Skowinski <g.skowinski@samsung.com>
2019-10-17 16:36:09 +0200

    Fixed .gitignore issue.

--- src/com/samsung/solutions/Krazki.java
+++ src/com/samsung/solutions/Krazki.java
@@ -1,22 +1,6 @@
 package com.samsung.solutions;
 
-import java.util.Scanner;
-
 public class Krazki {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        int n = scanner.nextInt();
-        int m = scanner.nextInt();
-        int[] r = new int[300000];
-        r[0] = scanner.nextInt();
-        for(int i = 1; i < n; ++i)
-            r[i] = Math.min(scanner.nextInt(), r[i - 1]);
-        int h = n;
-        for(int i = 0; i < m; ++i) {
-            int k = scanner.nextInt();
-            while(--h >= 0 && r[h] < k)
-                continue;
-        }
-        System.out.println(h > -1 ? h + 1 : 0);
     }
 }
