f59981e6863680e5ea2f4710d0548b5c7a185189
Grzegorz Skowinski <g.skowinski@samsung.com>
2019-11-03 17:30:14 +0100

    Time limits for Browar challenge corrected.

--- src/com/samsung/Main.java
+++ src/com/samsung/Main.java
@@ -7,7 +7,7 @@ import java.io.IOException;
 public class Main {
     public static void main(String[] args) throws IOException {
         // What tests do you want to run?
-        Tester.Tests test = Tester.Tests.Browar;
+        Tester.Tests test = Tester.Tests.All;
 
         // Do you want to see first received token that did not match expected one?
         Boolean verbose = true;
