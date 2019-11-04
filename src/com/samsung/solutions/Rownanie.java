78c0c414d7084c95e6ee09f6f55385058897d185
Grzegorz Skowinski <g.skowinski@samsung.com>
2019-11-04 22:25:43 +0100

    Added Rownanie class and input files for the challenge.

---
+++ src/com/samsung/solutions/Rownanie.java
@@ -0,0 +1,7 @@
+package com.samsung.solutions;
+
+// https://szkopul.edu.pl/problemset/problem/A7ZI0Wwn6tTiCJoYblblTAqz/site/
+public class Rownanie {
+    public static void main(String[] args) {
+    }
+}
