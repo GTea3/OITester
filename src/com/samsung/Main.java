40c79b5f53f1bd385f2e5a36949945eabfddb048
Grzegorz Skowinski <g.skowinski@samsung.com>
2019-11-04 22:05:30 +0100

    Challenge Tetris added.

--- src/com/samsung/Main.java
+++ src/com/samsung/Main.java
@@ -7,13 +7,13 @@ import java.io.IOException;
 public class Main {
     public static void main(String[] args) throws IOException {
         // What tests do you want to run?
-        Tester.Tests test = Tester.Tests.Tetris;
+        Tester.Tests test = Tester.Tests.All;
 
         // Do you want to see first received token that did not match expected one?
         Boolean verbose = true;
 
         // Do you want to stop testing as soon as a test fails?
-        Boolean stopOnFirstFail = false;
+        Boolean stopOnFirstFail = true;
 
         // Time limit multiplier (2.0f should be fine for Java)
         float timeLimitMultiplier = 3.0f;
--- src/com/samsung/solutions/Tetris.java
+++ src/com/samsung/solutions/Tetris.java
@@ -1,44 +1,7 @@
 package com.samsung.solutions;
 
-import java.util.Scanner;
-
 // https://szkopul.edu.pl/problemset/problem/noPY-IL0vsAi2TiXF-v2f5Br/site/
 public class Tetris {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-
-        int N = 50000; // maximum number of colors
-        int M = 1000000; // maximum answer size
-
-        int n = scanner.nextInt(); // input size
-        int[] d = new int[N]; int di = 0; // stack for elements that has been processed
-        int[] s = new int[2 * N]; int si = 2 * n; // stack for elements that has not been processed
-        int[] c = new int[N]; // seen, max N
-        int[] a = new int[M]; // answers, max M
-        int m = 0;
-        for(int i = 0; i < 2 * n; ++i)
-            s[2 * n - 1 - i] = scanner.nextInt() - 1;
-
-        while (si > 0) {
-            int x = s[--si];
-            if(c[x] == 0) {
-                d[di++] = x;
-                c[x] = 1;
-            }
-            else {
-                int j = di;
-                int y = d[--di];
-                c[y] = 0;
-                if(x != y) {
-                    a[m++] = j; // we're swapping j and j+1
-                    s[si++] = y;
-                    s[si++] = x;
-                }
-            }
-        }
-
-        System.out.println(m);
-        for(int i = 0; i < m; ++i)
-            System.out.println(a[i]);
     }
 }
