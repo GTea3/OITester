1c935f886f4aa4d9bdde98517c2f6a354974a91d
Grzegorz Skowinski <g.skowinski@samsung.com>
2020-02-14 13:14:23 +0100

    SWTAAirplaneGame solution cleanup done.

--- src/com/samsung/solutions/SWTAAirplaneGame.java
+++ src/com/samsung/solutions/SWTAAirplaneGame.java
@@ -7,36 +7,36 @@ public class SWTAAirplaneGame {
         Scanner scanner = new Scanner(System.in);
         int n = scanner.nextInt();
         int[][] map = new int[n][5];
-        for(int r = 0; r < n; ++r)
+        for(int r = n - 1; r >= 0; --r)
             for(int c = 0; c < 5; ++c)
                 map[r][c] = scanner.nextInt();
-        System.out.println(solve(map, n, 2, -1, 0));
+        System.out.println(Math.max(branch(map, -1, 2, -1, 0, true),
+                                    branch(map, -1, 2, -1, 0, false)));
     }
 
     private static int solve(int[][] map, int row, int col, int bomb, int score) {
-        if(row < 0)
+        if(row == map.length)
             return score;
 
-        if(row < map.length) {
-            if (map[row][col] == 1)
-                ++score;
-            if (map[row][col] == 2 && !(bomb != -1 && row > bomb - 5))
-                --score;
-            if (score < 0)
-                return -1;
-        }
+        if (map[row][col] == 1)
+            ++score;
+        if (map[row][col] == 2 && !(bomb != -1 && row < bomb + 5))
+            --score;
+        if (score < 0)
+            return -1;
 
         int best = -1;
-        for(int i = 0; i < 2; ++i) {
-            boolean useBomb = i > 0;
-            if(useBomb && bomb > -1)
-                continue;
-            for(int j = -1; j < 2; ++j) {
-                if(col + j < 0 || col + j >= 5)
-                    continue;
-                best = Math.max(best, solve(map, row - 1, col + j, useBomb ? row - 1 : bomb, score));
-            }
-        }
+        if(bomb == -1)
+            best = Math.max(best, branch(map, row, col, bomb, score, true));
+        best = Math.max(best, branch(map, row, col, bomb, score, false));
+        return best;
+    }
+
+    private static int branch(int[][] map, int row, int col, int bomb, int score, boolean useBomb) {
+        int best = -1;
+        for(int j = -1; j < 2; ++j)
+            if(col + j >= 0 && col + j < 5)
+                best = Math.max(best, solve(map, row + 1, col + j, useBomb ? row + 1 : bomb, score));
         return best;
     }
 }
